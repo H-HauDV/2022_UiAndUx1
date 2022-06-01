d82d259f6defbf9ed48bf5e2df32a243c7e8b612
H-HauDV <doanhau1407@gmail.com>
2022-06-01 16:32:01 +0700

    Dash board start

--- src/pages/Worker/report/index.jsx
+++ src/pages/Worker/report/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import Widget from '../../../components/widget'
+import Layout from '../../../components/layout'
 
 const Report = () => {
+ 
   return (
-    <div>
-      report
-    </div>
+    <Layout>
+      <div className="dashBoard">
+        <div className="widgets">
+          <Widget>
+            <div className="form">
+
+            </div>
+          </Widget>
+        </div>
+      </div>
+    </Layout>
   )
 }
 

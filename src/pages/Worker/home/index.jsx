85db2f90da9747cc0d3aa7314903f0e7491e890f
H-HauDV <doanhau1407@gmail.com>
2022-06-01 13:58:23 +0700

    Finish navbar css

--- src/pages/Worker/home/index.jsx
+++ src/pages/Worker/home/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Sidebar from '../../../components/sidebar'
+import Navbar from '../../../components/navbar'
+
 import "./home.scss"
 const Home = () => {
   return (
     <div className="home">
         <Sidebar/>
-        <div className="homeContainer">container</div>
+        <div className="homeContainer">
+          <Navbar/>
+          home container
+        </div>
     </div>
   )
 }
